28b4e473bcc8d06113b752830f09897f87e7bf39
matejslovak <joggurtik1@gmail.com>
2019-11-04 08:50:22 +0100

    Open_Lab_02._6

---
+++ Open-Lab-02.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_02._6
+{
+    class Program
+    {
+        public static bool Divisible(int input)
+        {
+            return input % 100 == 0;
+        }
+        static void Main(string[] args)
+        {
+            var input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Divisible(input));
+
+        }
+    }
+}
